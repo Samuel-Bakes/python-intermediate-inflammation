2b08938d558c3b210f083a622fe68d72c470a18f
Sam Bakes <sam.bakes@ukaea.uk>
2023-11-13 18:23:12 +0000

    add initial test cases for daily_max() and daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 import os
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -30,6 +30,35 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers"""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1,6],
+                          [4,3],
+                          [2,8]])
+    test_result = np.array([4,8])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers"""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1,6],
+                          [-4,3],
+                          [2,8]])
+    test_result = np.array([-4,3])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+        
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
     example_path = os.path.join(tmpdir, 'example.json')
