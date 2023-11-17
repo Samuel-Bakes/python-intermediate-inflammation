7b5e64f125fabfcd7ef25df60421314da4b92132
Sam Bakes <sam.bakes@ukaea.uk>
2023-11-17 14:13:42 +0000

    separated out functions and added tests to compute standard deviation

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -14,18 +14,27 @@ def analyse_data(data_dir):
     Gets all the inflammation csvs within a directory, works out the mean
     inflammation value for each day across all datasets, then graphs the
     standard deviation of these means."""
+    data = read_patient_data(data_dir)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+    graph_data = {
+        'standard deviation by day': daily_standard_deviation,
+    }
+    views.visualize(graph_data)
+
+    return daily_standard_deviation
+
+def compute_standard_deviation_by_day(data):
+    """Calculate the standard deviation by day between datasets"""
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
+
+def read_patient_data(data_dir):
+    """Load all of the patient data in a directory and return as an array"""
     data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError(f"No inflammation csv's found in path {data_dir}")
     data = map(models.load_csv, data_file_paths)
 
-
-    means_by_day = map(models.daily_mean, data)
-    means_by_day_matrix = np.stack(list(means_by_day))
-
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
-
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
-    }
-    views.visualize(graph_data)
+    return data
---
+++ tests/test_compute_data.py
@@ -0,0 +1,36 @@
+import numpy.testing as npt
+from pathlib import Path
+import glob
+import os
+import pytest
+import numpy as np
+
+from inflammation import models, views
+
+def test_analyse_data():
+    """Test for changes in behaviour by comparing current behaviour with old behaviour"""
+    from inflammation.compute_data import analyse_data
+    path = Path.cwd() / "data"
+    new_result = analyse_data(path)
+
+    data_file_paths = glob.glob(os.path.join(path, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation csv's found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+
+
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    old_result = np.std(means_by_day_matrix, axis=0)
+
+    npt.assert_array_equal(new_result, old_result)
+
+def test_standard_deviation():
+    from inflammation.compute_data import compute_standard_deviation_by_day
+    file = [[0, 1, 0],[0,2,0]]
+    input_data = [file]
+
+    result = compute_standard_deviation_by_day(input_data)
+
+    npt.assert_array_equal(result, [0,0,0])
