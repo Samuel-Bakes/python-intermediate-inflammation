008f760e75dce1e1f653fea9f707c6f42cde1c57
Sam Bakes <sam.bakes@ukaea.uk>
2023-11-23 15:57:15 +0000

    addressed comment on function name for standard_deviation

--- inflammation/models.py
+++ inflammation/models.py
@@ -55,12 +55,12 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
-def s_dev(data):
+def standard_deviation(data):
     """Computes and returns standard deviation for data."""
     mean = np.mean(data, axis=0)
     devs = []
     for entry in data:
-        devs.append((entry - mmm) * (entry - mmm))
+        devs.append((entry - mean) * (entry - mean))
 
     s_dev2 = sum(devs) / len(data)
     return {'standard deviation': s_dev2}
